bd6b9453b4baccbdcc63de53192a5b29337172ab
gexiaowei <gandxiaowei@gmail.com>
2014-11-19 14:40:18 +0800

    增加解析方法

--- script/jquery.istockanalysis.js
+++ script/jquery.istockanalysis.js
@@ -5,18 +5,111 @@
 
 (function ($) {
     var parents = [],
-        defaults = {};
+        analysis = function (el) {
+            var dom;
+            switch (el.prop("tagName")) {
+                case 'ITEM':
+                    dom = defaults.item(el);
+                    break;
+                case 'STOCK':
+                    dom = defaults.stock(el);
+                    break;
+                case 'FONT':
+                    dom = defaults.font(el);
+                    break;
+                default :
+                    dom = defaults.unknow(el);
+                    break;
+            }
+            return dom;
+        },
+        /**
+         * 处理根节点
+         * @param el
+         * @returns {*|HTMLElement}
+         * @private
+         */
+        _analysis_item = function (el) {
+            var dom = $('<li/>', {class: 'item'});
+            dom.text(el.text().trim());
+            return dom;
+        },
+        /**
+         * 处理股票标签
+         * @param el
+         * @returns {XMLList|*}
+         * @private
+         */
+        _analysis_stock = function (el) {
+            var name = el.attr('name'),
+                code = el.attr('code'),
+                dom = $('<a/>', {
+                    class: 'stock',
+                    href: 'stockinfo.html?codeid=' + code + '&codename=' + name
+                }).text(name);
+            return dom;
+        },
+        /**
+         * 处理额外的标签
+         * @param el
+         * @returns {XMLList|*}
+         * @private
+         */
+        _analysis_font = function (el) {
+            var text = el.attr('text'),
+                color = el.attr('color'),
+                dom = $('<li/>', {
+                    class: 'font'
+                });
+            dom.css({color: color});
+            dom.text(text.replace(' ', '：'));
+            return dom;
+        },
+        /**
+         * 处理未知标签
+         * @param el
+         * @returns {XMLList|*}
+         * @private
+         */
+        _analysis_unknow = function (el) {
+            var dom = $('<li/>', {
+                class: 'unknow'
+            });
+            dom.text(el.text());
+            return dom;
+        },
+        /**
+         * 默认设置
+         * @type {{item: Function, stock: Function, font: Function, unknow: Function}}
+         */
+        defaults = {
+            item: _analysis_item,
+            stock: _analysis_stock,
+            font: _analysis_font,
+            unknow: _analysis_unknow
+        };
 
-    $.fn.analysisStock = function (istock, options) {
-        if (!istock) {
+    /**
+     * jQuery插件方法——分析iStock
+     * @param data
+     * @param options
+     * @returns {$.fn}
+     */
+    $.fn.analysisStock = function (data, options) {
+        if (!data) {
             return;
         }
+        var istock = data.content,
+            time = data.ctime,
+            type = data.stype;
+
         if (options) {
-            defaults.root = options.root || _analysis_root;
-            defaults.item = options.item || _analysis_item;
-            defaults.stock = options.stock || _analysis_stock;
-            defaults.unknow = options.unknow || _analysis_unknow;
+            var analysisfn;
+            for (analysisfn in options) {
+                defaults[analysisfn] = options[analysisfn];
+            }
         }
+
         var items = istock.split('\n'),
             root = $('<root/>');
         $(items).each(function (i, item) {
@@ -24,60 +117,41 @@
                 root.append($('<item>' + item + '</item>'));
             }
         });
+        this.addClass('istock');
+        $('<div/>', {class: 'istocktype'}).appendTo(this);
+        var istockContainer = $('<ul/>');
+        $('<li/>', {class: 'date'}).appendTo(istockContainer).text(time)
+        istockContainer.appendTo(this);
+        parents.push(istockContainer);
         loopElement(root);
+        return this;
     };
 
+    /**
+     * 是否包含子节点
+     * @returns {boolean}
+     */
     $.fn.hasChildren = function () {
         return this.children().length > 0;
     }
 
+    /**
+     * 循环调用
+     * @param root
+     */
     var loopElement = function (root) {
         if (!root || !$(root).hasChildren()) {
             return;
         }
         $(root).children().each(function (i, child) {
-            analysis($(child));
+            var dom = analysis($(child));
+            dom.appendTo(parents[parents.length - 1]);
+            if ($(child).hasChildren()) {
+                parents.push(dom);
+            }
             loopElement(child);
         });
-    };
-
-    var analysis = function (el) {
-        switch (el.prop("tagName")) {
-            case 'ITEM':
-                defaults.item(el);
-                break;
-            case 'STOCK':
-                defaults.stock(el);
-                break;
-            default :
-                defaults.unknow(el);
-                break;
-        }
-    };
-
-    var _analysis_root = function (el) {
-        var dom = $('<div/>');
-        return dom;
-    };
-
-    var _analysis_item = function (el) {
-        var dom = $('<div/>');
-        return dom;
-    };
-
-    var _analysis_stock = function (el) {
-        var dom = $('<div/>'),
-            name = el.attr('name'),
-            code = el.attr('code');
-        dom.bind('click', function () {
-
-        });
-        return dom;
-    };
-
-    var _analysis_unknow = function (el) {
-        var dom = $('<div/>');
-        return dom;
+        parents.pop();
     };
 
 })(jQuery);
