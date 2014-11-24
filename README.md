092cf9c31a076087a0feaaa7b642cd22819f548c
gexiaowei <gandxiaowei@gmail.com>
2014-11-24 17:17:08 +0800

    增加一些常用工具类方法

--- README.md
+++ README.md
@@ -13,9 +13,37 @@ var httprequest = $.galhttprequset(url,params,callbacks);
 ```js
 httprequest.requst();        //普通网络请求
 httprequest.requestPacket(); //Packet请求
-httprequest.submitData()     //POST提交数据
-httprequest.submitForm()     //提交表单数据
+httprequest.submitData();    //POST提交数据
+httprequest.submitForm();    //提交表单数据
 ```
-PS:
-1.所有网络请求中的参数将会以base64加密后的数据进行传输
-2.所有请求中都会增加ak、userid、sessionid三个请求头
+注意事项:
+所有网络请求中的参数将会以base64加密后的数据进行传输<br/>
+所有请求中都会增加ak、userid、sessionid三个请求头
+
+###2.Base64加密解密
+```js
+var base = $.base64();  //创建base64
+base.encode(string);    //加密一个字符串
+base.decode(string);    //解密一个字符串
+```
+这里ase64用了10个随机策略对字符串进行base64的加密和解密
+
+###3.本地数据的存储
+```js
+var localData = $.localData(key); //从本地存储中获取对象
+localData.get();                  //获取对应key的值 如果可以用json解析，则自动解析
+localData.set(value);             //设置对应key的值
+localData.remove();               //移除对应key的值
+```
+
+###4.从地址栏中获取参数
+```js
+var params = $.getParams() //获取地址栏中的参数值
+```
+如果地址栏中没有参数返回一个空的Object值
+
+###5.格式化排名
+```js
+var rankstr = $.formatRank(rank) //格式化牛人排名
+```
+排名小于99999返回该值，大于99999且小于999999 返回万+ 大于999999 返回100万+
--- src/script/jquery.jhss.tool.js
+++ src/script/jquery.jhss.tool.js
@@ -1,10 +1,131 @@
+/*!
+ * jQuery Cookie Plugin v1.4.1
+ * https://github.com/carhartl/jquery-cookie
+ *
+ * Copyright 2013 Klaus Hartl
+ * Released under the MIT license
+ */
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // CommonJS
+        factory(require('jquery'));
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+
+    var pluses = /\+/g;
+
+    function encode(s) {
+        return config.raw ? s : encodeURIComponent(s);
+    }
+
+    function decode(s) {
+        return config.raw ? s : decodeURIComponent(s);
+    }
+
+    function stringifyCookieValue(value) {
+        return encode(config.json ? JSON.stringify(value) : String(value));
+    }
+
+    function parseCookieValue(s) {
+        if (s.indexOf('"') === 0) {
+            // This is a quoted cookie as according to RFC2068, unescape...
+            s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
+        }
+
+        try {
+            // Replace server-side written pluses with spaces.
+            // If we can't decode the cookie, ignore it, it's unusable.
+            // If we can't parse the cookie, ignore it, it's unusable.
+            s = decodeURIComponent(s.replace(pluses, ' '));
+            return config.json ? JSON.parse(s) : s;
+        } catch (e) {
+        }
+    }
+
+    function read(s, converter) {
+        var value = config.raw ? s : parseCookieValue(s);
+        return $.isFunction(converter) ? converter(value) : value;
+    }
+
+    var config = $.cookie = function (key, value, options) {
+
+        // Write
+
+        if (value !== undefined && !$.isFunction(value)) {
+            options = $.extend({}, config.defaults, options);
+
+            if (typeof options.expires === 'number') {
+                var days = options.expires,
+                    t = options.expires = new Date();
+                t.setTime(+t + days * 864e+5);
+            }
+
+            return (document.cookie = [
+                encode(key), '=', stringifyCookieValue(value),
+                options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+                options.path ? '; path=' + options.path : '',
+                options.domain ? '; domain=' + options.domain : '',
+                options.secure ? '; secure' : ''
+            ].join(''));
+        }
+
+        // Read
+
+        var result = key ? undefined : {};
+
+        // To prevent the for loop in the first place assign an empty array
+        // in case there are no cookies at all. Also prevents odd result when
+        // calling $.cookie().
+        var cookies = document.cookie ? document.cookie.split('; ') : [];
+
+        for (var i = 0, l = cookies.length; i < l; i++) {
+            var parts = cookies[i].split('=');
+            var name = decode(parts.shift());
+            var cookie = parts.join('=');
+
+            if (key && key === name) {
+                // If second argument (value) is a function it's a converter...
+                result = read(cookie, value);
+                break;
+            }
+
+            // Prevent storing a cookie that we couldn't decode.
+            if (!key && (cookie = read(cookie)) !== undefined) {
+                result[name] = cookie;
+            }
+        }
+
+        return result;
+    };
+
+    config.defaults = {};
+
+    $.removeCookie = function (key, options) {
+        if ($.cookie(key) === undefined) {
+            return false;
+        }
+
+        // Must not alter options, thus extending a fresh object...
+        $.cookie(key, '', $.extend({}, options, {
+            expires: -1
+        }));
+        return !$.cookie(key);
+    };
+
+}));
 /**
  * JHSS 工具库
  * Created by Vivian on 14/11/21.
  * Copyright gandxiaowei@gmail.com 2014;
  */
 ;
-(function ($) {
+(function ($, window) {
     "use strict";
     /**
      *
@@ -628,6 +749,102 @@
         }
     };
 
+    /**
+     * 格式化排名
+     * @param   {String|Number} rank
+     * @returns {String}
+     */
+    function FormatRank(rank) {
+        if (typeof rank === 'string') {//把字符转换成数字
+            rank = parseInt(rank);
+        }
+        var rankStr;
+        if (rank == 0) {
+            rankStr = "";
+        } else if (rank > 99999 && rank <= 999999) {
+            rankStr = (Math.floor(rank / 10000)).toFixed(0) + "万+";
+        } else if (rank > 999999) {
+            rankStr = "100万+";
+        } else {
+            rankStr = rank.toFixed(0);//toFixed只能用于数字
+        }
+        return rankStr;
+    };
+
+    /**
+     * 从地址栏中获取参数
+     * @returns {Object}
+     */
+    function getParamsFromUrl() {
+        var paramstr = window.location.search,
+            params = new Object();
+        if (paramstr) {
+            $(paramstr.substr(1).split('&')).each(function (i, item) {
+                var kv = item.split('='),
+                    key = kv[0],
+                    value = kv[1];
+                params[key] = value;
+            });
+        }
+        return params;
+    };
+
+    /**
+     * 获取本地存储对象
+     * @param {String} key
+     */
+    function localData(key) {
+        this.key = key;
+    };
+
+    localData.prototype = {
+        /**
+         * 获取对应key的值
+         * @returns {Object|String}
+         */
+        get: function () {
+            var value;
+            try {
+                localStorage.setItem('test', '1');
+                localStorage.remove('test');
+                value = localStorage.getItem(this.key);
+            } catch (e) {
+                value = $.cookie(this.key);
+            }
+            try {
+                return JSON.parse(value);
+            } catch (e) {
+                return value;
+            }
+        },
+        /**
+         * 设置对应key的值
+         * @param {Oject|String} value
+         */
+        set: function (value) {
+            if (typeof value === 'object') {
+                value = JSON.stringify(value);
+            }
+            try {
+                localStorage.setItem(this.key, value);
+            } catch (e) {
+                $.cookie(this.key, value);
+            }
+        },
+        /**
+         * 移除对应key的值
+         */
+        remove: function () {
+            try {
+                localStorage.setItem('test', '1');
+                localStorage.remove('test');
+                localStorage.remove(this.key);
+            } catch (e) {
+                $.removeCookie(this.key);
+            }
+        }
+    };
+
     $.extend({
         galhttprequset: function (url, params, callback) {
             return new GalHttpequest(url, params, callback);
@@ -637,8 +854,13 @@
         },
         base64: function () {
             return new Base64();
-        }
-    })
+        },
+        localData: function (key) {
+            return new localData(key)
+        },
+        formatRank: FormatRank,
+        getParams: getParamsFromUrl
 
+    });
 })
-(jQuery);
+(jQuery, window);
